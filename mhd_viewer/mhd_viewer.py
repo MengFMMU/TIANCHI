5acd79a0da2afefba9c6e1faaecfe8bb90c3cb04
lixx11 <lxx2011011580@gmail.com>
2017-05-26 10:48:21 +0800

    add npz format support

--- mhd_viewer/mhd_viewer.py
+++ mhd_viewer/mhd_viewer.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import glob
+import datetime
 
 from PyQt4 import uic, QtCore, QtGui
 from PyQt4.QtGui import QMainWindow
@@ -10,6 +11,7 @@ import pyqtgraph as pg
 from pyqtgraph.parametertree import ParameterTree, Parameter
 
 import numpy as np
+import pandas as pd
 from math import sqrt
 import SimpleITK as sitk
 
@@ -39,6 +41,90 @@ def getFilepathFromLocalFileID(localFileID):  # get real filepath from POSIX fil
   return QtCore.QUrl(str(absCFURL[0])).toLocalFile()
 
 
+def makeTextItem(text):
+  """Make table item with text centered"""
+  item = QtGui.QTableWidgetItem(text)
+  item.setTextAlignment(QtCore.Qt.AlignCenter)
+  return item
+
+
+def makeNumItem(num):
+  item = QtGui.QTableWidgetItem()
+  item.setData(0, float(num))
+  return item
+
+
+class NoduleTable(QtGui.QDialog):
+  def __init__(self, parent=None):
+    super(NoduleTable, self).__init__(parent)
+    dir_ = os.path.dirname(os.path.abspath(__file__))
+    uic.loadUi(dir_ + '/' + 'table.ui', self)
+    # add slot for buttons
+    self.noduleCSV = None
+    self.noduleDir = None
+    self.df = None
+
+    # add slot for table
+    self.table.cellClicked.connect(
+      self.onCellClickedSlot)
+
+  def updateTable(self, csv=None, noduleDir=None):
+    self.table.setRowCount(0)  # clear all rows
+    self.noduleCSV = csv 
+    self.noduleDir = noduleDir
+    df = pd.read_csv(self.noduleCSV)
+    # fill table with df
+    for i in range(len(df)):
+      row = df.iloc[i].copy()
+      print('insert row:', row)
+      self.table.insertRow(i)
+      # seriesuid
+      self.table.setItem(i, 0, 
+        makeTextItem('%s' % row[0]))
+      # coorX
+      self.table.setItem(i, 1, 
+        makeNumItem(row[1]))
+      # coorY
+      self.table.setItem(i, 2, 
+        makeNumItem(row[2]))
+      # coorZ
+      self.table.setItem(i, 3, 
+        makeNumItem(row[3]))
+      # diameter
+      self.table.setItem(i, 4, 
+        makeNumItem(row[4]))
+      # filepath
+      fpath = glob.glob('%s/*/%s.mhd' % (
+        self.noduleDir, row[0]))[0]
+      print(datetime.datetime.now())
+      print(row)
+      print(fpath)
+      self.table.setItem(i, 5, 
+        makeTextItem('%s' % fpath))
+
+  def onCellClickedSlot(self, row, column):
+    item = self.table.item(row, 5)
+    fpath = str(item.text())
+    coorX = float(self.table.item(row, 1).text())
+    coorY = float(self.table.item(row, 2).text())
+    coorZ = float(self.table.item(row, 3).text())
+    diameter = float(self.table.item(row, 4).text())
+    # connect to parent
+    parent = self.parent()
+    parent.filepath = fpath
+    parent.params.param('Nodule', 
+      'coorX').setValue(coorX)
+    parent.params.param('Nodule', 
+      'coorY').setValue(coorY)
+    parent.params.param('Nodule', 
+      'coorZ').setValue(coorZ)
+    parent.params.param('Nodule', 
+      'diameter').setValue(diameter)
+    parent.changeFileSlot()
+    parent.params.param('Nodule', 
+      'Go to nodule').activate()
+    
+
 class MHDViewer(QMainWindow):
   def __init__(self, parent=None):
     super(MHDViewer, self).__init__(parent)
@@ -46,7 +132,7 @@ class MHDViewer(QMainWindow):
     uic.loadUi(dir_ + '/' + 'layout.ui', self)
     # accept drop mhd files
     self.setAcceptDrops(True)
-    self.acceptedFileTypes = ('mhd',)
+    self.acceptedFileTypes = ('mhd', 'npz')
 
     self.filepath = None  # mhd filepath
     self.data = None  # mhd 3d array
@@ -65,6 +151,20 @@ class MHDViewer(QMainWindow):
     self.noduleDiameter = 0.  # nodule diameter in mm
     self.noduleItem = pg.ScatterPlotItem()
     self.imageView.getView().addItem(self.noduleItem)
+
+    # mask
+    self.applyMask = False
+    self.minThres = -600
+    self.maxThres = 200
+
+    # nodule csv
+    self.noduleTable = NoduleTable(parent=self)
+    self.noduleCSV = None
+    self.noduleDir = ''
+
+    # setup menu slots
+    self.actionOpenCSV.triggered.connect(self.openCSVSlot)
+    self.actionNoduleTable.triggered.connect(self.noduleTableSlot)
 
     # parameter tree
     params_list = [
@@ -86,13 +186,21 @@ class MHDViewer(QMainWindow):
               {'name': 'diameter', 'type': 'float', 'value': self.noduleDiameter},
               {'name': 'Go to nodule', 'type': 'action'},
             ]},
+            {'name': 'Mask', 'type': 'group', 'children': [
+              {'name': 'Apply Mask', 'type': 'bool', 'value': self.applyMask},
+              {'name': 'Min Threshold', 'type': 'float', 'value': self.minThres},
+              {'name': 'Max Threshold', 'type': 'float', 'value': self.maxThres},
+            ]},
+            {'name': 'Advanced Options', 'type': 'group', 'children': [
+              {'name': 'mhd directory', 'type': 'str', 'value': self.noduleDir},
+            ]},
             ]
     self.params = Parameter.create(name='params', 
       type='group', children=params_list)
     self.parameterTree.setParameters(self.params, showTop=False)
 
     # signal and slot
-    self.fileList.itemDoubleClicked.connect(self.changeFileSlot)
+    self.fileList.itemDoubleClicked.connect(self.itemClicked)
     self.imageView.scene.sigMouseMoved.connect(self.mouseMoved)
     self.params.param('Basic Operation', 
       'Axis').sigValueChanged.connect(self.axisChangedSlot)
@@ -110,6 +218,14 @@ class MHDViewer(QMainWindow):
       'diameter').sigValueChanged.connect(self.diameterChangedSlot)
     self.params.param('Nodule', 
       'Go to nodule').sigActivated.connect(self.goToNoduleSlot)
+    self.params.param('Mask',
+      'Apply Mask').sigValueChanged.connect(self.applyMaskSlot)
+    self.params.param('Mask',
+      'Min Threshold').sigValueChanged.connect(self.setMinThresSlot)
+    self.params.param('Mask',
+      'Max Threshold').sigValueChanged.connect(self.setMaxThresSlot)
+    self.params.param('Advanced Options', 
+      'mhd directory').sigValueChanged.connect(self.setNoduleDirSlot)
 
     # disable old ROI and create new ROI
     self.imageView.roi.sigRegionChanged.disconnect(self.imageView.roiChanged)
@@ -118,6 +234,31 @@ class MHDViewer(QMainWindow):
     self.imageView.view.addItem(self.imageView.roi)
     self.imageView.roi.hide()
     self.imageView.roi.sigRegionChanged.connect(self.roiChanged)
+
+  def applyMaskSlot(self, _, applyMask):
+    self.applyMask = applyMask
+    self.updateDisplay()
+
+  def setMinThresSlot(self, _, minThres):
+    print('min thres: ', minThres)
+    self.minThres = minThres
+
+  def setMaxThresSlot(self, _, maxThres):
+    print('max thres: ', maxThres)
+    self.maxThres = maxThres
+
+  def setNoduleDirSlot(self, _, noduleDir):
+    self.noduleDir = noduleDir
+
+  def openCSVSlot(self):
+    fpath = str(QtGui.QFileDialog.getOpenFileName(self, 
+      'Open CSV', '', 'CSV File (*.csv)'))
+    self.noduleCSV = fpath
+
+  def noduleTableSlot(self):
+    self.noduleTable.updateTable(csv=self.noduleCSV,
+      noduleDir=self.noduleDir)
+    self.noduleTable.exec_()
 
   def roiChanged(self):
     if self.data is None:
@@ -163,15 +304,22 @@ class MHDViewer(QMainWindow):
     self.fileList.insertItem(0, item)
 
   def updateDisplay(self):
-    if self.data is  None:
+    data = self.data.copy()
+    if data is  None:
       return
+    if self.applyMask:
+      data *= self.mask
     if self.axis == 'z':
-      image = self.data[self.frame,:,:]
+      image = data[self.frame,:,:]
     elif self.axis == 'y':
-      image = self.data[:,self.frame,:]
+      image = data[:,self.frame,:]
     elif self.axis == 'x':
-      image = self.data[:,:,self.frame]
+      image = data[:,:,self.frame]
     self.image = image.T
+    # if self.applyMask:  # make lung mask
+      # mask = (self.image > self.minThres) * (self.image < self.maxThres)
+      # self.mask = mask
+      # self.image *= self.mask
     self.imageView.setImage(self.image, 
       autoRange=False, autoLevels=False, autoHistogramRange=True)
     if self.showNodule:
@@ -189,13 +337,26 @@ class MHDViewer(QMainWindow):
           size=2. * circle_radius_voxel, symbol='o', 
           brush=(255,255,255,0), pen='r', pxMode=False)
 
-  def changeFileSlot(self, item):
-    # load mhd file and update display
+  def itemClicked(self, item):
     self.filepath = item.filepath
-    itk_img = sitk.ReadImage(str(self.filepath))
-    self.data = sitk.GetArrayFromImage(itk_img)  # indexes are z, y, x
-    self.origin = np.array(itk_img.GetOrigin())
-    self.spacing = np.array(itk_img.GetSpacing())
+    self.changeFileSlot()
+
+  def changeFileSlot(self):
+    # load CT file and update display
+    ext = os.path.splitext(self.filepath)[-1]
+    if ext == '.mhd':
+      itk_img = sitk.ReadImage(str(self.filepath))
+      self.data = sitk.GetArrayFromImage(itk_img)  # indexes are z, y, x
+      self.origin = np.array(itk_img.GetOrigin())
+      self.spacing = np.array(itk_img.GetSpacing())
+    elif ext == '.npz':
+      data = np.load(self.filepath)
+      self.data = data['data']
+      self.origin = data['origin']
+      self.spacing = data['spacing']
+      self.mask = data['mask']
+    else:
+      print('Error! %s is not supported!' % ext)
     self.updateDisplay()
     self.roiChanged()
 
