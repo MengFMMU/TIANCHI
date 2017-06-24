952cceac688bd5a91ecfdf9345a243abd1eff0f9
lixx11 <lxx2011011580@gmail.com>
2017-06-24 09:53:49 +0800

    add cnn3d for fine classification

---
+++ cnn3d/cnn3d_input.py
@@ -0,0 +1,301 @@
+#!/usr/bin/env python
+
+import numpy as np 
+import tensorflow as tf
+from scipy.ndimage.interpolation import map_coordinates
+import pandas as pd
+import random
+from math import pi, sin, cos
+
+import time
+from glob import glob
+import os
+
+
+ROTATION_RANGE = [0, 360]
+STRENTCH_RANGE = [0.8, 1.2]
+POS_SHIFT_RANGE = [0., 0.1]  # relative shift range for positive samples
+NEG_SHIFT_RANGE = [0, 2]  # absolute shift range for negative samples
+NOISE_RANGE = [0.8, 1.2] 
+
+class CNN3dTrainInput(object):
+    """docstring for LUNATrainInput"""
+    def __init__(self, 
+                 data_dir,
+                 annotation_file, 
+                 prediction_file, 
+                 min_nodule=10, 
+                 max_nodule=100,
+                 macro_batch_size=10,
+                 macro_batch_reuse=100,
+                 micro_batch_size=100,
+                 sample_ratio=[0.5,0.5],
+                 sample_size=48,  # image sample size in x,y,z dimention
+                 debug=False,
+                 verbose=False):
+        self.data_dir = data_dir
+        self.annotation_file = annotation_file  # manually annotation csv file 
+        self.prediction_file = prediction_file  # predicted nodule csv file
+        self.min_nodule = min_nodule  # in mm
+        self.max_nodule = max_nodule  # in mm
+        self.macro_batch_size = macro_batch_size
+        self.macro_batch_reuse = macro_batch_reuse
+        self.micro_batch_size = micro_batch_size
+        self.sample_ratio = sample_ratio  # sampling ratio of nodule, tissue, border
+        self.sample_size = sample_size
+        self.debug = debug
+        self.verbose = verbose
+
+        # get positive samples from annotation file
+        ref_df = pd.read_csv(self.annotation_file)
+        diameters = ref_df['diameter_mm'].values
+        pos_idx = np.where((diameters > self.min_nodule) * \
+            (diameters < self.max_nodule))[0]
+        self.pos_rows = ref_df.iloc[pos_idx]
+        if self.debug:
+            print('positive samples: \n', self.pos_rows)
+
+        # get negative samples from prediction file
+        pred_df = pd.read_csv(self.prediction_file)
+        diameters = pred_df['diameter_mm'].values
+        neg_idx = np.where(diameters == 0.)[0]
+        self.neg_rows = pred_df.iloc[neg_idx]
+        if self.debug:
+            print('negative samples: \n', self.neg_rows)
+
+        # collect seriesuids
+        self.seriesuids = []
+        self.seriesuids += list(set(self.pos_rows['seriesuid'].values.tolist()))
+        # self.seriesuids += list(set(self.neg_rows['seriesuid'].values.tolist()))
+
+        # init macro and micro batch
+        self.macro_batch = None
+        self.macro_batch_count = 0
+        self.micro_batch = None
+
+    def get_sample(self, image, x, y, z, 
+        sample_type=None, radius_px=0.):
+        sample = None
+        s_z, s_y, s_x = image.shape
+        ss_x, ss_y, ss_z = [self.sample_size] * 3
+
+        # data augmentation
+        angle = float(random.randint(ROTATION_RANGE[0], 
+            ROTATION_RANGE[1])) / 180. * pi
+        strentch_ratio_x = random.uniform(STRENTCH_RANGE[0], 
+            STRENTCH_RANGE[1])
+        strentch_ratio_y = random.uniform(STRENTCH_RANGE[0], 
+            STRENTCH_RANGE[1])
+        if sample_type == 'P': # positive sample
+            shift_x = random.uniform(POS_SHIFT_RANGE[0]*radius_px, 
+                POS_SHIFT_RANGE[1]*radius_px)
+            shift_y = random.uniform(POS_SHIFT_RANGE[0]*radius_px, 
+                POS_SHIFT_RANGE[1]*radius_px)
+        elif sample_type == 'N':  # negative sample
+            shift_x = random.uniform(NEG_SHIFT_RANGE[0], 
+                NEG_SHIFT_RANGE[1])
+            shift_y = random.uniform(NEG_SHIFT_RANGE[0], 
+                NEG_SHIFT_RANGE[1])
+        flip_rv = random.randint(0, 4)
+
+        try:
+            for offset in range(int(-ss_z//2), int(-ss_z//2+ss_z)):
+                slice_y, slice_x = np.indices(image.shape[1:])
+                crop_y = slice_y[int(y-ss_y//2):int(y-ss_y//2+ss_y),
+                                 int(x-ss_x//2):int(x-ss_x//2+ss_x)]
+                crop_x = slice_x[int(y-ss_y//2):int(y-ss_y//2+ss_y),
+                                 int(x-ss_x//2):int(x-ss_x//2+ss_x)]
+                _y = (crop_y - y).reshape(-1)
+                _x = (crop_x - x).reshape(-1)
+                _xy = np.vstack((_x, _y))
+                RM = np.array([[cos(angle), -sin(angle)],
+                               [sin(angle), cos(angle)]])
+                _rxy = RM.dot(_xy) 
+                crop_x = np.reshape(_rxy[0], crop_x.shape) + x
+                crop_y = np.reshape(_rxy[1], crop_y.shape) + y
+                crop_x = (crop_x - x) / strentch_ratio_x + x
+                crop_y = (crop_y - y) / strentch_ratio_y + y 
+                crop_x += shift_x
+                crop_y += shift_y           
+
+                sample_slice = image[int(z+offset),:,:]
+                crop_slice = map_coordinates(sample_slice, [crop_y, crop_x], order=0)
+                if flip_rv == 2:
+                    crop_slice = np.flipud(crop_slice)
+                elif flip_rv == 3:
+                    crop_slice == np.fliplr(crop_slice)
+                if sample is None:
+                    sample = crop_slice[np.newaxis,...]
+                else:
+                    sample = np.vstack((sample, crop_slice[np.newaxis,...]))
+        except:
+            if self.debug or self.verbose:
+                print('catch exception during sample generation')
+            return None
+        # add some noise
+        noise = np.random.uniform(low=NOISE_RANGE[0], 
+            high=NOISE_RANGE[1], size=sample.shape)
+        sample = (sample.astype(np.float32) * noise.astype(np.float32)).astype(np.int16)
+        return sample
+
+    def next_micro_batch(self):
+        if self.debug:
+            start = time.time()
+        if self.macro_batch is None or \
+            self.macro_batch_count == self.macro_batch_reuse:
+            self.next_macro_batch()
+            self.macro_batch_count = 0
+
+        count = 0
+        samples = []
+        labels = []
+
+        nb_pos_samples = int(self.micro_batch_size * self.sample_ratio[0])
+        nb_neg_samples = int(self.micro_batch_size * self.sample_ratio[1])
+
+        # positive nodule samples
+        for i in range(nb_pos_samples):
+            # randomly pick a CT scan
+            idx = random.randint(0, len(self.macro_batch)-1)
+            seriesuid, data = self.macro_batch[idx]
+            spacing = data['spacing']  # in x, y, z order
+            origin = data['origin']  # in x, y, z order
+            image = data['image']  # in z, y, x order
+
+            # randomly pick a nodule in this CT record
+            rows = self.pos_rows[self.pos_rows['seriesuid'] == seriesuid]
+            if len(rows) == 0:
+                if self.debug or self.verbose:
+                    print('skipping %s for positive sample generation' %
+                        seriesuid)
+                continue
+            idx = random.randint(0, len(rows)-1)
+            row = rows.iloc[idx]
+            if self.debug or self.verbose:
+                print('generating positive sample from %s' % 
+                    str(row))
+            coord_mm = np.asarray([row['coordX'], row['coordY'], row['coordZ']])
+            diameter = row['diameter_mm']
+            r = int(diameter / spacing[0] / 2)  # radius in pixel
+            coord_pixel = (coord_mm - origin) / spacing
+            x, y, z = coord_pixel
+            sample = self.get_sample(image, x, y, z,
+                sample_type='P', radius_px=r)
+            if sample is None:
+                continue
+            samples.append(sample)
+            labels.append(1)  # 1 for nodule, 0 for else
+            count += 1
+
+            if self.debug:
+                print('saving %d sample' % count)
+                np.save('positive_sample_%d.npy' % count, sample)
+
+        # negative samples
+        for i in range(nb_neg_samples):
+            # randomly pick a CT scan
+            idx = random.randint(0, len(self.macro_batch)-1)
+            seriesuid, data = self.macro_batch[idx]
+            spacing = data['spacing']  # in x, y, z order
+            origin = data['origin']  # in x, y, z order
+            image = data['image']  # in z, y, x order
+
+            # randomly pick a false positive in this CT scan
+            rows = self.neg_rows[self.neg_rows['seriesuid'] == seriesuid]
+            if len(rows) == 0:
+                if self.debug or self.verbose:
+                    print('skipping %s for negative sample generation' %
+                        seriesuid)
+                continue
+            idx = random.randint(0, len(rows)-1)
+            row = rows.iloc[idx]
+            if self.debug or self.verbose:
+                print('generating negative sample from %s' % 
+                    str(row))
+            coord_mm = np.asarray([row['coordX'], row['coordY'], row['coordZ']])
+            coord_pixel = (coord_mm - origin) / spacing
+            x, y, z = coord_pixel
+            sample = self.get_sample(image, x, y, z,
+                sample_type='N')
+            if sample is None:
+                continue
+            samples.append(sample)
+            labels.append(0)  # 1 for nodule, 0 for else
+            count += 1
+
+            if self.debug:
+                print('saving %d sample' % count)
+                np.save('negative_sample_%d.npy' % count, sample)
+
+        # add some samples if not enough
+        nb_dummy_samples = self.micro_batch_size - len(samples)
+        if self.debug or self.verbose:
+            print('generating %d dummy samples' % nb_dummy_samples)
+        for i in range(nb_dummy_samples):
+            sample = samples[0]
+            dummy_sample = np.ones_like(sample) * -1000
+            samples.append(dummy_sample)  # dummy nodule
+            labels.append(0)
+
+        self.macro_batch_count += 1
+        if self.debug:
+            end = time.time()
+            time_cost = end - start
+            print('time elapsed loading micro batch: %.2f sec' % time_cost)
+        samples = np.asarray(samples, dtype=np.float32)
+        samples = samples.reshape((self.micro_batch_size, 
+            self.sample_size, self.sample_size, self.sample_size, 1))
+        labels = np.asarray(labels, dtype=np.int8)
+        self.micro_batch = [samples, labels]
+        return self.micro_batch
+
+    def next_macro_batch(self):
+        if self.debug or self.verbose:
+            start = time.time()
+        macro_batch_size = min(self.macro_batch_size, len(self.seriesuids))
+        random_idx = random.sample(range(len(self.seriesuids)), macro_batch_size)
+        self.macro_batch = []
+        for i in random_idx:
+            seriesuid = self.seriesuids[i]
+            fname = '%s/%s.npz' % (self.data_dir, seriesuid)
+            if self.debug or self.verbose:
+                print 'loading %s' % fname
+            data = np.load(fname)
+            spacing = data['spacing']
+            origin = data['origin']
+            image = data['data']
+            mask = data['mask']
+            data = {'spacing': spacing,
+                    'origin': origin,
+                    'image': image,
+                    'mask': mask}
+            self.macro_batch.append((seriesuid, data))
+        if self.debug or self.verbose:
+            end = time.time()
+            time_cost = end - start
+            print('time elapsed loading macro batch: %.2f sec' % time_cost)
+
+
+if __name__ == '__main__':
+    data_dir = '/Volumes/SPIDATA/TIANCHI/train_processed_border-5_gradient-search'
+    annotation_file = '/Volumes/SPIDATA/TIANCHI/csv/train/annotations.csv'
+    prediction_file = '/Users/lixuanxuan/Desktop/TIANCHI/train_prediction/train-0608-min-dist_10.csv'
+    show = True
+    train_input = CNN3dTrainInput(data_dir, 
+                                annotation_file,
+                                prediction_file,
+                                macro_batch_size=3,
+                                min_nodule=30, 
+                                max_nodule=100, 
+                                sample_ratio=[0.5,0.5],
+                                debug=False,
+                                verbose=True)
+    data, labels = train_input.next_micro_batch()
+    if show:
+        import matplotlib.pyplot as plt
+
+        rv = random.randint(0, len(data))
+        plt.imshow(data[rv][0,:,:], cmap='gray')
+        plt.title(labels[rv])
+        plt.clim((-600, 200))
+        plt.show()
---
+++ cnn3d/cnn3d_train.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+
+
+import tensorflow as tf 
+import numpy as np
+from math import pow
+
+import cnn3d
+from cnn3d_input import CNN3dTrainInput
+
+import time
+from datetime import datetime
+from glob import glob
+
+FLAGS = tf.app.flags.FLAGS
+
+# Basic model parameters.
+tf.app.flags.DEFINE_integer('macro_batch_size', 10,
+                            """Number of scan to process in a macro batch.""")
+tf.app.flags.DEFINE_integer('macro_batch_reuse', 10,
+                            """Number of macro batch recycling.""")
+tf.app.flags.DEFINE_string('sample_ratio', '0.5,0.5',
+                            """Sample ratio of nodule, tissue and border samples""")
+tf.app.flags.DEFINE_string('train_dir', 'train',
+                           """Directory where to write event logs """
+                           """and checkpoint.""")
+tf.app.flags.DEFINE_integer('max_steps', 1000000,
+                            """Number of batches to run.""")
+tf.app.flags.DEFINE_float('learning_rate', 0.01,
+                            """Initial learning rate.""")
+tf.app.flags.DEFINE_integer('decay_steps', 10,
+                            """Decay step for learning rate.""")
+tf.app.flags.DEFINE_float('decay_factor', 0.99,
+                            """Decay factor for learning rate.""")
+tf.app.flags.DEFINE_boolean('log_device_placement', False,
+                            """Whether to log device placement.""")
+tf.app.flags.DEFINE_integer('log_frequency', 5,
+                            """How often to log results to the console.""")
+tf.app.flags.DEFINE_boolean('shuffle', True,
+                            """Whether to shuffle the batch.""")
+tf.app.flags.DEFINE_boolean('load_ckpt', False,
+                            """Whether to load checkpoint file.""")
+tf.app.flags.DEFINE_integer('ckpt_step', 0,
+                            """Global step of ckpt file.""")
+
+
+SAMPLE_SIZE = 48
+
+
+def train():
+    batch_size = FLAGS.batch_size
+    sample_ratio = map(float, FLAGS.sample_ratio.split(','))
+
+    with tf.Graph().as_default():
+        global_step = tf.Variable(0, name='global_step', trainable=False)
+
+        # input
+        train_input = CNN3dTrainInput(FLAGS.data_dir, 
+                                    FLAGS.annotation_file, 
+                                    FLAGS.prediction_file,
+                                    min_nodule=FLAGS.min_nodule, 
+                                    max_nodule=FLAGS.max_nodule,
+                                    micro_batch_size=batch_size,
+                                    macro_batch_size=FLAGS.macro_batch_size,
+                                    macro_batch_reuse=FLAGS.macro_batch_reuse,
+                                    sample_ratio=sample_ratio,
+                                    sample_size=SAMPLE_SIZE,
+                                    debug=FLAGS.debug,
+                                    verbose=FLAGS.verbose)
+        if FLAGS.use_fp16:
+            FP = tf.float16
+        else:
+            FP = tf.float32
+        images = tf.placeholder(FP, 
+                shape=[batch_size, SAMPLE_SIZE, SAMPLE_SIZE, SAMPLE_SIZE, 1], 
+                name='image')
+        labels = tf.placeholder(tf.int64, shape=[batch_size], name='label')
+        # Display the training images in the visualizer.
+        
+        image_slices = tf.slice(images, 
+            [0,SAMPLE_SIZE//2,0,0,0], 
+            [int(batch_size), 1, SAMPLE_SIZE, SAMPLE_SIZE, 1],
+            name='central_slice')
+        image_slices = tf.reshape(image_slices, 
+            (int(batch_size), SAMPLE_SIZE, SAMPLE_SIZE, 1))
+        tf.summary.image('image_slices', image_slices, max_outputs=10)
+
+        # inference
+        logits = cnn3d.inference(images)
+
+        # calculate accuracy and error rate
+        correct_prediction = tf.equal(tf.argmax(logits,1), labels)
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        error_rate = 1 - accuracy
+        tf.summary.scalar('accuracy', accuracy)
+        tf.summary.scalar('error_rate', error_rate)
+
+        # calculate FP/FN ratio
+        _positive = tf.equal(labels, 1)
+        _negative = tf.equal(labels, 0)
+        TP = tf.reduce_sum(tf.multiply(tf.cast(correct_prediction, tf.float32), 
+                         tf.cast(_positive, tf.float32)))
+        TN = tf.reduce_sum(tf.multiply(tf.cast(correct_prediction, tf.float32),
+                         tf.cast(_negative, tf.float32)))
+        FN_ratio = 1. - tf.divide(TP, tf.reduce_sum(tf.cast(_positive, tf.float32)))
+        FP_ratio = 1. - tf.divide(TN, tf.reduce_sum(tf.cast(_negative, tf.float32)))
+        tf.summary.scalar('FP_ratio', FP_ratio)
+        tf.summary.scalar('FN_ratio', FN_ratio)
+
+        # train to minimize loss
+        loss = cnn3d.loss(logits, labels)
+        lr = tf.placeholder(tf.float64, name='leaning_rate')
+        tf.summary.scalar('learning_rate', lr)
+        train_op = cnn3d.train(loss, lr, global_step)
+
+        # Add ops to save and restore all the variables.
+        saver = tf.train.Saver()
+
+        # run graph in session
+        with tf.Session() as sess:
+            init = tf.global_variables_initializer() # create an operation initializes all the variables
+            sess.run(init)
+            merged = tf.summary.merge_all()
+            writer = tf.summary.FileWriter('%s' % FLAGS.train_dir, sess.graph)
+            
+            if FLAGS.load_ckpt:
+                ckpt_file = '%s/model.ckpt-%d' % \
+                    (FLAGS.train_dir, FLAGS.ckpt_step)
+                print('restore sess with %s' % ckpt_file)
+                saver.restore(sess, ckpt_file)
+
+            start = time.time()
+            for step in range(FLAGS.max_steps):
+                batch_images, batch_labels = train_input.next_micro_batch()
+                if FLAGS.shuffle:
+                    idx = np.arange(FLAGS.batch_size)
+                    np.random.shuffle(idx)
+                    batch_images = batch_images[idx]
+                    batch_labels = batch_labels[idx]
+                lr_value = FLAGS.learning_rate * pow(FLAGS.decay_factor, 
+                    (step / FLAGS.decay_steps))
+                _, err, g_step, loss_value, correctness, summary = sess.run(
+                    [train_op, error_rate, global_step, loss, correct_prediction, merged], 
+                    feed_dict={
+                        labels: batch_labels,
+                        images: batch_images,
+                        lr: lr_value,
+                    })
+
+                if step % FLAGS.log_frequency == 0 and step != 0:
+                    end = time.time()
+                    duration = end - start
+                    examples_per_sec = FLAGS.log_frequency * FLAGS.batch_size / duration
+                    sec_per_batch = float(duration / FLAGS.log_frequency)
+                    format_str = ('%s: step %d, loss = %.2f, err = %.4f (%.1f examples/sec; %.3f '
+                        'sec/batch)')
+                    print (format_str % (datetime.now(), g_step, loss_value, err, 
+                               examples_per_sec, sec_per_batch))
+                    writer.add_summary(summary, g_step)
+                    # Save the variables to disk.
+                    saver.save(sess, '%s/model.ckpt' % FLAGS.train_dir,  global_step=g_step)
+                    start = end
+
+
+def main(argv=None):  # pylint: disable=unused-argument
+    tf.gfile.MakeDirs(FLAGS.train_dir)
+    train()
+
+
+if __name__ == '__main__':
+    tf.app.run()
